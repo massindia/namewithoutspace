747ae4353db1920667f688757d595fe6e9a72f7c
massindia <mr.perfect@live.co.uk>
2016-12-05 12:14:08 +0530

    Namewithoutspace
    without space name

---
+++ src/namewithoutsemicolon.java
@@ -0,0 +1,19 @@
+
+
+public class namewithoutsemicolon 
+{
+	public static void main(String[] args)
+		{
+		
+//here i using print output using in for loop to print any string without semicolon      
+        for(int i=1 ;i < 2; System.out.println("Mass India"))
+
+        	{
+
+            	i++;
+
+        	}
+			
+		}
+}
+		
